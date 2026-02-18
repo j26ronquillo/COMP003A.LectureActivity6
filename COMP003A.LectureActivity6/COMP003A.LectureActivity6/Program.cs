f04281d70785a494c6426e85f0e742268777ed2c
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-02-17 21:52:39 -0800

    Reached step 9 and chanegd the age field

---
+++ COMP003A.LectureActivity6/COMP003A.LectureActivity6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Cache;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace COMP003A.LectureActivity6
+{
+    class Student
+    {
+        public string Name;
+        public int Age;
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void DisplayInfo() 
+        { Console.WriteLine($"Student Name: {Name}, Age: {Age}"); }
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Stephen", 36);
+            Student student2 = new Student("Josh", 18);
+
+            student1.DisplayInfo();
+			student2.DisplayInfo();
+		}
+
+	}
+}
